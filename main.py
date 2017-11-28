1653437cd76dc586fc10808a780c66b4d455e5a0
isaac-davis-15 <isaacdaviswa2000@gmail.com>
2017-11-27 16:43:01 -0800

    Test

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ crashed = False
 """
 white = (255, 255, 255)
 
-speed = 6		
+speed = 12		
 robot1X = display_width/4
 robot1Y = display_height/2
 
@@ -59,24 +59,47 @@ while not crashed:
 			
 	#Check keyboard and change the cord.
 	#==============================================
+	
+	top_border = display_height/2 - 240
+	bottom_border = display_height/2 + 240
+	right_border = display_width/2 + 240
+	left_border = display_width/2 - 240
+	
 	keyHandler = pygame.key.get_pressed()
-	if(keyHandler[273]): #Up
+	if(keyHandler[273] and robot1Y >= top_border + speed): #Up
 		robot1Y -= speed
-	elif(keyHandler[274]): #Down
+	elif(keyHandler[274] and robot1Y <= (bottom_border - 24) - speed): #Down
 		robot1Y += speed
-	if(keyHandler[275]): #Right
+	if(keyHandler[275] and robot1X <= (right_border - 24) - speed): #Right
 		robot1X += speed
-	elif(keyHandler[276]): #Left
+	elif(keyHandler[276] and robot1X >= left_border + speed): #Left
 		robot1X -= speed
 	
-	if(keyHandler[97]):
+	if(keyHandler[97] and robot2X >= left_border + speed): 
+		print("LEFT")
 		robot2X -= speed
-	if(keyHandler[115]):
+	elif(keyHandler[115] and robot2Y <= (bottom_border - 24) - speed):
+		print("DOWN")
 		robot2Y += speed
-	if(keyHandler[100]):
+	if(keyHandler[100] and robot2X <= (right_border - 24) - speed): 
+		print("RIGHT")
 		robot2X += speed
-	if(keyHandler[119]):
+	elif(keyHandler[119] and robot2Y >= top_border + speed):
+		print("UP")
 		robot2Y -= speed
+	
+	#if(keyHandler[97]):
+	#	print("lEFT")
+	#	robot2X -= speed
+	#if(keyHandler[115]):
+	#	print("DOWN")
+	#	robot2Y += speed
+	#if(keyHandler[100]):
+	#	print("RIGHT")
+	#	robot2X += speed
+	#if(keyHandler[119]):
+	#	print("UP")
+	#	robot2Y -= speed
 		
 
 	#Update frames and player cord. 
@@ -92,18 +115,14 @@ while not crashed:
 	
 	#check bounds
 	#===============================================
-	top_border = display_height/2 - 240
-	bottom_border = display_height/2 + 240
-	right_border = display_width/2 + 240
-	left_border = display_width/2 - 240
 	
-	if(robot1X <= left_border):
+	if():
 		robot1X = left_border
-	if(robot1X >= right_border - 24):
+	if():
 		robot1X = right_border - 24 #Acounting the size of the image 
-	if(robot1Y <= top_border):
+	if():
 		robot1Y = top_border
-	if(robot1Y >= bottom_border - 24):
+	if():
 		robot1Y = bottom_border - 24 #Acounting the size of the image 
 	
 	if(robot2X <= left_border):
